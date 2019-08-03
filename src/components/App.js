538ad6df3720a51d986836312f511889cacd1e16
John <johnrubio93@gmail.com>
2019-08-03 10:59:22 +1000

    setup basic language toggle app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 
 class App extends React.Component {
+    state = { language: "english" };
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    };
+
     render() {
-        return <div>App</div>;
+        return (
+            <div>
+                <div>
+                    <div>Select a language: </div>
+                    <button onClick={() => this.onLanguageChange("english")}>English</button>
+                    <button onClick={() => this.onLanguageChange("japanese")}>Japanese</button>
+                </div>
+                <p>{this.state.language}</p>
+            </div>
+        );
     }
 }
 
