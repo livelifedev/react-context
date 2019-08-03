f35856e25ff983b84a0bb32cfe82cd94ce4e76d7
John <johnrubio93@gmail.com>
2019-08-03 12:27:33 +1000

    setup provider in app to pass state data down

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
 
 class App extends React.Component {
     state = { language: "english" };
@@ -16,7 +17,9 @@ class App extends React.Component {
                     <button onClick={() => this.onLanguageChange("english")}>English</button>
                     <button onClick={() => this.onLanguageChange("japanese")}>Japanese</button>
                 </div>
-                <UserCreate />
+                <LanguageContext.Provider value={this.state.language}>
+                    <UserCreate />
+                </LanguageContext.Provider>
             </div>
         );
     }
